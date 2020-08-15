2bc1e7929524a17cf36e8a809d31e78a29301ba9
Matthieu Le brazidec (r3v2d0g) <r3v2d0g@jesus.gg>
2020-08-15 14:33:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Alias for [futures-micro](https://github.com/irrustible/futures-micro)
+
+[![img](https://img.shields.io/crates/l/ufut.svg)](https://github.com/r3v2d0g/ufut/blob/main/LICENSE.txt) [![img](https://img.shields.io/crates/v/ufut.svg)](https://crates.io/crates/ufut) [![img](https://docs.rs/ufut/badge.svg)](https://docs.rs/ufut)
+
+This crate only provides an alias for [`futures-micro`](https://github.com/irrustible/futures-micro), please visit its repository to say thank you and/or fuck you to its author <3.
+
+
+## License
+
+> This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at <http://mozilla.org/MPL/2.0/>.
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+/**************************************************************************************************
+ *                                                                                                *
+ * This Source Code Form is subject to the terms of the Mozilla Public                            *
+ * License, v. 2.0. If a copy of the MPL was not distributed with this                            *
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.                                       *
+ *                                                                                                *
+ **************************************************************************************************/
+
+//! This crate only provides an alias for [`futures-micro`], please visit its repository to say
+//! thank you and/or fuck you to its author <3.
+//!
+//! [`futures-micro`]: https://github.com/irrustible/futures-micro
+
+pub use futures_micro::*;
